a6e4a55f10d8ecd2c7ab4b3c963dca12639eab9e
jesusriverouep-design <jesusrivero.uep@gmail.com>
2026-05-13 15:21:49 -0400

    Update productos.js

--- productos.js
+++ productos.js
@@ -19,4 +19,44 @@ const productos = [
             "Frecuencia": "120Hz - 10kHz"
         }
     }
+    {
+        id: 2,
+        modelo: "PS-S6",
+        marca: "Power Su",
+        nombre: "Par de medio rangos\"",
+        precio: 45,
+        precioAnterior: 60,
+        status: "popular",
+        stock: true,
+        categoria: "MEDIOS 6.5",
+        imagenes: ["img/PS-S6-1.png", "img/PS-S6-2.png", "img/PS-S6-3.png"],
+        descripcion: "Altavoces de gama media de alta eficiencia.",
+        specs: {
+            "Potencia Max": "400W",
+            "Potencia RMS": "200W",
+            "Impedancia": "4 Ohm",
+            "Sensibilidad": "95dB",
+            "Frecuencia": "120Hz - 10kHz"
+        }
+    }
+      {
+        id: 3,
+        modelo: "PS-S6",
+        marca: "Power Su",
+        nombre: "Par de medio rangos\"",
+        precio: 45,
+        //precioAnterior: 60,
+        status: "",
+        stock: false,
+        categoria: "MEDIOS 6.5",
+        imagenes: ["img/PS-S6-1.png", "img/PS-S6-2.png", "img/PS-S6-3.png"],
+        descripcion: "Altavoces de gama media de alta eficiencia.",
+        specs: {
+            "Potencia Max": "400W",
+            "Potencia RMS": "200W",
+            "Impedancia": "4 Ohm",
+            "Sensibilidad": "95dB",
+            "Frecuencia": "120Hz - 10kHz"
+        }
+    }
 ];
