1604dbcf8c8e8f113b4a3245ea132ab073a85fb9
jesusriverouep-design <jesusrivero.uep@gmail.com>
2026-05-13 15:16:18 -0400

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,22 @@
+const productos = [
+   {
+        id: 1,
+        modelo: "PS-S6",
+        marca: "Power Su",
+        nombre: "Par de medio rangos\"",
+        precio: 45,
+        //precioAnterior: 45,
+        status: "popular",
+        stock: true,
+        categoria: "MEDIOS 6.5",
+        imagenes: ["img/PS-S6-1.png", "img/PS-S6-2.png", "img/PS-S6-3.png"],
+        descripcion: "Altavoces de gama media de alta eficiencia.",
+        specs: {
+            "Potencia Max": "400W",
+            "Potencia RMS": "200W",
+            "Impedancia": "4 Ohm",
+            "Sensibilidad": "95dB",
+            "Frecuencia": "120Hz - 10kHz"
+        }
+    }
+];
